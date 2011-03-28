64f2690e0cbbdf20641a84939264181bb0be6c58
Ludwinas <ludwinas@gmail.com>
2011-03-29 00:18:35 +0200

    modified scraper

--- scraper.py
+++ scraper.py
@@ -1,17 +1,43 @@
 #scraper.py
-
+import math
+import string
 from lxml import etree
 
-URL = "http://ludwinas.tumblr.com/api/read/"
-
-tree = etree.parse(URL)
-root = tree.getroot()
-
-total = root.find("posts").attrib['total']
-
-for post in root.iterfind(".//post"):
-	print post.attrib['url']
-
+iURL = "http://ludwinas.tumblr.com/api/read/"
+URL = ""
+urlList = []
+current = 0
+step = 20
+currentIteration = 0
+#GLOBAL
+maxIterations = 0
 
 
+def getTotal():
+	global maxIterations
+	root = etree.parse(iURL).getroot()
+	total = root.find("posts").attrib['total']
+	maxIterations = math.floor(int(total)/step)
+	maxIterations = int(maxIterations)
+
+getTotal()
+
+def iterate(URL):
+	root = etree.parse(URL).getroot()
+	for post in root.iterfind(".//post"):
+		urlList.append(post.attrib['url'])
+	currentIteration+1
+
+#current = len(urlList)
+print currentIteration
+print maxIterations
+
+print currentIteration < maxIterations
+
+if currentIteration < maxIterations:
+	print "di"
+	num = str(step*currentIteration)
+	URL = iURL+"?start="+num
+	print URL
+#	iterate(URL)
 
