e61751f5637b4d87e1debc0ccbd47ab1f4857976
Ludwinas <ludwinas@gmail.com>
2011-03-28 23:40:30 +0200

    modified scraper

--- scraper.py
+++ scraper.py
@@ -1,3 +1,17 @@
 #scraper.py
 
+from lxml import etree
+
+URL = "http://ludwinas.tumblr.com/api/read/"
+
+tree = etree.parse(URL)
+root = tree.getroot()
+
+total = root.find("posts").attrib['total']
+
+for post in root.iterfind(".//post"):
+	print post.attrib['url']
+
+
+
 
